8230c18c967837985a7a8d8497c61563bfc384fc
ayhappy99 <76838783+ayhappy99@users.noreply.github.com>
2022-07-04 17:54:32 +0900

    Add files via upload

---
+++ code03-02.py
@@ -0,0 +1,23 @@
+'''sel = int(input("입력 진수 결정 (16//10/8/2/):"))
+num = input("값 인력: ")
+
+if sel ==16 :
+    num10 = int(num, 16)
+if sel ==10 :
+    num10 = int(num, 10)
+if sel ==8 :
+    num10 = int(num, 8)
+if sel == 2:
+    num10 = int(num, 2)
+
+print("16진수 ==>", hex(num10))
+print("10진수 ==>", num10)
+print("8진수 ==>", oct(num10))
+print("2진수 ==>", bin(num10))'''
+
+
+print("[메뉴]\n아메리카노:2,000원 카페라떼:3,000원 카푸치노:3500원")
+print("아메리카노 판매 개수:",input("")
+print("카페라떼 판매 개수:")
+print("카푸치노 판매 개수:")
+print("총 매출은"%d "입니다.")
