dc927a641f496a722c910494820312b81267045b
ayhappy99 <76838783+ayhappy99@users.noreply.github.com>
2022-07-04 17:55:21 +0900

    Add files via upload

---
+++ 학점입력.py
@@ -0,0 +1,13 @@
+score = int(input("a 값 입력하시오:"))
+
+if score >=90 :
+    print("A")
+elif score >=80:
+    print("B")
+elif score >=70:
+    print("C")
+elif score >=60:
+    print("D")
+else :
+    print("F")
+print("학점입니다.^^") 
